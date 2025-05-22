9373eed877df2b08ee3acc266e5b48ea624821de
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-05-22 23:07:19 +0600

    fixed some issue

--- src/pages/CreateGroups/CreateGroups.jsx
+++ src/pages/CreateGroups/CreateGroups.jsx
@@ -1,9 +1,10 @@
-import { use } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router";
+import swal from "sweetalert";
 
 export const CreateGroups = () => {
-  const { user, isDark } = use(AuthContext);
+  const { user, isDark } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleCreateGroup = (e) => {
@@ -21,12 +22,11 @@ export const CreateGroups = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        if(data.insertedId){
-          swal(" Group Created Successfully!",);
-          navigate("/allGroups")
+        if (data.insertedId) {
+          swal("Group Created Successfully!");
+          navigate("/allGroups");
         }
       });
-
   };
 
   return (
@@ -40,18 +40,29 @@ export const CreateGroups = () => {
       <form
         onSubmit={handleCreateGroup}
         className={`backdrop-blur-lg border p-5 md:p-8 rounded-xl w-full max-w-xl shadow-2xl ${
-          isDark
-            ? "bg-white/5 border-white/20"
-            : "bg-white/10 border-white/50"
+          isDark ? "bg-white/5 border-white/20" : "bg-white/10 border-white/50"
         }`}
       >
         <h2 className="text-2xl font-bold text-center mb-6">Create a Group</h2>
 
-        <label className="block mb-2 font-medium">Group Name:<span className="text-red-500">*</span></label>
-        <input type="text" name="groupName" className="w-full p-2 mb-4 border rounded bg-transparent" required />
+        <label className="block mb-2 font-medium">
+          Group Name:<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          name="groupName"
+          className="w-full p-2 mb-4 border rounded bg-transparent"
+          required
+        />
 
-        <label className="block mb-2 font-medium">Hobby Category:<span className="text-red-500">*</span></label>
-        <select name="hobbyCategory" required className="w-full p-2 mb-4 border rounded bg-transparent">
+        <label className="block mb-2 font-medium">
+          Hobby Category:<span className="text-red-500">*</span>
+        </label>
+        <select
+          name="hobbyCategory"
+          required
+          className="w-full p-2 mb-4 border rounded bg-transparent"
+        >
           <option value="Drawing & Painting">Drawing & Painting</option>
           <option value="Photography">Photography</option>
           <option value="Video Gaming">Video Gaming</option>
@@ -64,7 +75,9 @@ export const CreateGroups = () => {
 
         <div className="flex md:flex-row flex-col gap-2 mb-4">
           <div className="w-full">
-            <label className="block mb-2 font-medium">User Name:<span className="text-red-500">*</span></label>
+            <label className="block mb-2 font-medium">
+              User Name:<span className="text-red-500">*</span>
+            </label>
             <input
               type="text"
               name="userName"
@@ -75,7 +88,9 @@ export const CreateGroups = () => {
             />
           </div>
           <div className="w-full">
-            <label className="block mb-2 font-medium">User Email:<span className="text-red-500">*</span></label>
+            <label className="block mb-2 font-medium">
+              User Email:<span className="text-red-500">*</span>
+            </label>
             <input
               type="email"
               name="email"
@@ -89,23 +104,60 @@ export const CreateGroups = () => {
 
         <div className="flex md:flex-row flex-col gap-2 mb-4">
           <div className="w-full">
-            <label className="block mb-2 font-medium">Meeting Location:<span className="text-red-500">*</span></label>
-            <input type="text" name="location" className="w-full p-2 border rounded bg-transparent" required />
+            <label className="block mb-2 font-medium">
+              Meeting Location:<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              name="location"
+              className="w-full p-2 border rounded bg-transparent"
+              required
+            />
           </div>
           <div className="w-full">
-            <label className="block mb-2 font-medium">Max Members:<span className="text-red-500">*</span></label>
-            <input type="text" name="members" className="w-full p-2 border rounded bg-transparent" required />
+            <label className="block mb-2 font-medium">
+              Max Members:<span className="text-red-500">*</span>
+            </label>
+            <input
+              type="number"
+              name="members"
+              min="1"
+              className="w-full p-2 border rounded bg-transparent"
+              required
+            />
           </div>
         </div>
 
-        <label className="block mb-2 font-medium">End Date:<span className="text-red-500">*</span></label>
-        <input type="date" name="date" className="w-full p-2 mb-4 border rounded bg-transparent" required />
+        <label className="block mb-2 font-medium">
+          End Date:<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="date"
+          name="date"
+          min={new Date().toISOString().split("T")[0]}
+          className="w-full p-2 mb-4 border rounded bg-transparent"
+          required
+        />
 
-        <label className="block mb-2 font-medium">Image URL:<span className="text-red-500">*</span></label>
-        <input type="text" name="photo" className="w-full p-2 mb-4 border rounded bg-transparent" required />
+        <label className="block mb-2 font-medium">
+          Image URL:<span className="text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          name="photo"
+          className="w-full p-2 mb-4 border rounded bg-transparent"
+          required
+        />
 
-        <label className="block mb-2 font-medium">Description:<span className="text-red-500">*</span></label>
-        <input type="text" name="description" className="w-full p-2 mb-4 border rounded bg-transparent" required />
+        <label className="block mb-2 font-medium">
+          Description:<span className="text-red-500">*</span>
+        </label>
+        <textarea
+          name="description"
+          rows="4"
+          className="w-full p-2 mb-4 border rounded bg-transparent"
+          required
+        ></textarea>
 
         <button
           type="submit"
--- src/pages/UpdateGroup/UpdateGroup.jsx
+++ src/pages/UpdateGroup/UpdateGroup.jsx
@@ -22,175 +22,183 @@ export const UpdateGroup = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        if(data.matchedCount){
+        if (data.matchedCount) {
           swal({
-              title: "Group Updated Successfully",
-              icon: "success",
-              button: "Ok",
-            });
+            title: "Group Updated Successfully",
+            icon: "success",
+            button: "Ok",
+          });
+          navigate("/myGroups")
         }
       });
   };
 
   return (
-    <section className={isDark
+    <section
+      className={
+        isDark
           ? "bg-gradient-to-br via-blue-950 from-gray-900 to-black text-gray-200"
-          : "bg-gradient-to-br from-purple-200 via-blue-100 to-blue-200"}>
-        <div
-      className={`min-h-screen container mx-auto px-3 md:px-6 lg:px-8 xl:px-14  p-4 `}
+          : "bg-gradient-to-br from-purple-200 via-blue-100 to-blue-200"
+      }
     >
-      <button onClick={()=>navigate(-1)} className="btn btn-sm border-none bg-rose-700 text-white">Go Back</button>
-      <form
-        onSubmit={handleUpdateGroup}
-        className={`p-5 mx-auto mt-10 md:p-8 rounded-xl w-full max-w-xl shadow-xl backdrop-blur-md ${
-          isDark
-            ? "bg-black/20 border border-white/10"
-            : "bg-white/10 border border-black/10"
-        }`}
-      >
-        <h2
-          className={`text-2xl font-bold text-center mb-6 ${
-            isDark ? "text-white" : "text-gray-800"
+      <div className="min-h-screen container mx-auto px-3 md:px-6 lg:px-8 xl:px-14 p-4">
+        <button
+          onClick={() => navigate(-1)}
+          className="btn btn-sm border-none bg-rose-700 text-white"
+        >
+          Go Back
+        </button>
+        <form
+          onSubmit={handleUpdateGroup}
+          className={`p-5 mx-auto my-10 md:p-8 rounded-xl w-full max-w-xl shadow-xl backdrop-blur-md ${
+            isDark
+              ? "bg-black/20 border border-white/10"
+              : "bg-white/10 border border-black/10"
           }`}
         >
-          Update Your Group
-        </h2>
+          <h2
+            className={`text-2xl font-bold text-center mb-6 ${
+              isDark ? "text-white" : "text-gray-800"
+            }`}
+          >
+            Update Your Group
+          </h2>
 
-        {/* Group Name */}
-        <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-          Group Name:<span className="text-red-500">*</span>
-        </label>
-        <input
-          type="text"
-          name="groupName"
-          defaultValue={groupData.groupName}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
+          {/* Group Name */}
+          <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+            Group Name:<span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="groupName"
+            defaultValue={groupData.groupName}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
 
-        {/* Hobby Category */}
-        <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-          Hobby Category:<span className="text-red-500">*</span>
-        </label>
-        <select
-          className="w-full p-2 mb-4 border rounded"
-          name="hobbyCategory"
-          defaultValue={groupData.hobbyCategory}
-          required
-        >
-          <option value="Drawing & Painting">Drawing & Painting</option>
-          <option value="Photography">Photography</option>
-          <option value="Video Gaming">Video Gaming</option>
-          <option value="Fishing">Fishing</option>
-          <option value="Running">Running</option>
-          <option value="Cooking">Cooking</option>
-          <option value="Reading">Reading</option>
-          <option value="Writing">Writing</option>
-        </select>
+          {/* Hobby Category */}
+          <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+            Hobby Category:<span className="text-red-500">*</span>
+          </label>
+          <select
+            className="w-full p-2 mb-4 border rounded"
+            name="hobbyCategory"
+            defaultValue={groupData.hobbyCategory}
+            required
+          >
+            <option value="Drawing & Painting">Drawing & Painting</option>
+            <option value="Photography">Photography</option>
+            <option value="Video Gaming">Video Gaming</option>
+            <option value="Fishing">Fishing</option>
+            <option value="Running">Running</option>
+            <option value="Cooking">Cooking</option>
+            <option value="Reading">Reading</option>
+            <option value="Writing">Writing</option>
+          </select>
 
-        {/* User Info */}
-        <div className="flex md:flex-row flex-col gap-2 mb-4">
-          <div className="w-full">
-            <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-              User Name:<span className="text-red-500">*</span>
-            </label>
-            <input
-              type="text"
-              name="userName"
-              value={groupData.userName}
-              readOnly
-              className="w-full p-2 border rounded "
-              required
-            />
+          {/* User Info */}
+          <div className="flex md:flex-row flex-col gap-2 mb-4">
+            <div className="w-full">
+              <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+                User Name:<span className="text-red-500">*</span>
+              </label>
+              <input
+                type="text"
+                name="userName"
+                value={groupData.userName}
+                readOnly
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+                User Email:<span className="text-red-500">*</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={groupData.email}
+                readOnly
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
           </div>
-          <div className="w-full">
-            <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-              User Email:<span className="text-red-500">*</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={groupData.email}
-              readOnly
-              className="w-full p-2 border rounded"
-              required
-            />
+
+          {/* Location and Members */}
+          <div className="flex md:flex-row flex-col gap-2 mb-4">
+            <div className="w-full">
+              <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+                Meeting Location:<span className="text-red-500">*</span>
+              </label>
+              <input
+                type="text"
+                name="location"
+                defaultValue={groupData.location}
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+                Max Members:<span className="text-red-500">*</span>
+              </label>
+              <input
+                type="text"
+                name="members"
+                defaultValue={groupData.members}
+                className="w-full p-2 border rounded"
+                required
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Location and Members */}
-        <div className="flex md:flex-row flex-col gap-2 mb-4">
-          <div className="w-full">
-            <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-              Meeting Location:<span className="text-red-500">*</span>
-            </label>
-            <input
-              type="text"
-              name="location"
-              defaultValue={groupData.location}
-              className="w-full p-2 border rounded"
-              required
-            />
-          </div>
-          <div className="w-full">
-            <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-              Max Members:<span className="text-red-500">*</span>
-            </label>
-            <input
-              type="text"
-              name="members"
-              defaultValue={groupData.members}
-              className="w-full p-2 border rounded"
-              required
-            />
-          </div>
-        </div>
+          {/* Date */}
+          <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+            Start Date:<span className="text-red-500">*</span>
+          </label>
+          <input
+            type="date"
+            name="date"
+            defaultValue={groupData.date}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
 
-        {/* Date */}
-        <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-          Start Date:<span className="text-red-500">*</span>
-        </label>
-        <input
-          type="date"
-          name="date"
-          defaultValue={groupData.date}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
+          {/* Photo URL */}
+          <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+            Image URL:<span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="photo"
+            defaultValue={groupData.photo}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
 
-        {/* Photo URL */}
-        <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-          Image URL:<span className="text-red-500">*</span>
-        </label>
-        <input
-          type="text"
-          name="photo"
-          defaultValue={groupData.photo}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
+          {/* Description (textarea) */}
+          <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
+            Description:<span className="text-red-500">*</span>
+          </label>
+          <textarea
+            name="description"
+            defaultValue={groupData.description}
+            rows="4"
+            className="w-full p-2 mb-4 border rounded"
+            required
+          ></textarea>
 
-        {/* Description */}
-        <label className={`block mb-2 font-medium ${isDark ? "text-white" : "text-gray-700"}`}>
-          Description:<span className="text-red-500">*</span>
-        </label>
-        <input
-          type="text"
-          name="description"
-          defaultValue={groupData.description}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
-
-        {/* Submit */}
-        <button
-          type="submit"
-          className="btn w-full bg-purple-500 text-white font-bold py-2 rounded border-none hover:bg-purple-600 transition"
-        >
-          Update
-        </button>
-      </form>
-    </div>
+          {/* Submit */}
+          <button
+            type="submit"
+            className="btn w-full bg-purple-500 text-white font-bold py-2 rounded border-none hover:bg-purple-600 transition"
+          >
+            Update
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
