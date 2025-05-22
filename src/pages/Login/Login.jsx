a18e52e912ef82c2ad81f7e0a1269285d49bfee3
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-05-22 12:52:06 +0600

    added error message at login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router"
 import { AuthContext } from "../../context/AuthContext";
 
@@ -7,11 +7,12 @@ export const Login = ()=>{
     const {signinUser , googleSignin ,} = use(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
+    const [errorMessage , setErrorMessage] = useState(false)
 
     console.log(location);
 
     const handleSignIn = (e)=>{
-
+        setErrorMessage(false)
         e.preventDefault();
         const form = e.target ; 
         const formData = new FormData(form);
@@ -19,7 +20,9 @@ export const Login = ()=>{
 
         signinUser(email , password).then((result)=>{
             navigate(`${location.state || "/"}`)
-        }).catch(error => console.log(error));
+        }).catch((error)=>{
+            setErrorMessage(true);
+        });
 
         
     }
@@ -27,7 +30,7 @@ export const Login = ()=>{
     const handleGoogleRegister = ()=>{
         googleSignin().then((result)=>{
             navigate(`${location.state || "/"}`);
-        }).catch(erroe => console.log(erroe))
+        }).catch(erroe => console.log(erroe.message))
     }
 
 
@@ -53,6 +56,9 @@ export const Login = ()=>{
                   <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                   Login with Google
                 </button>
+                <div>
+                    {errorMessage && <p className="text-rose-600 font-medium text-sm text-center">Invalid Credential</p>}
+                </div>
             </div>
         </div>
 
