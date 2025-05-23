4dc0a9f0dac6d1bec40c359f7001a790cbcdc85b
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-05-24 01:53:55 +0600

    last and final commit after check

--- src/components/GroupCards/GroupCard.jsx
+++ src/components/GroupCards/GroupCard.jsx
@@ -11,14 +11,14 @@ export const GroupCard = ({singleGroupData})=>{
                <div className="relative  p-6 bg-white/10 backdrop-blur-lg border border-white/20 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-300 group">
                  <div className="absolute top-0 right-0 w-20 h-20 rounded-bl-3xl rounded-tr-2xl bg-gradient-to-br from-purple-500 to-pink-500 opacity-20 group-hover:opacity-40 transition duration-300" />
                  
-                 <div className="flex flex-col items-center text-center">
+                 <div className="flex flex-col items-start text-center">
                    <img
                      src={singleGroupData.photo}
                      alt="Avatar"
-                     className="w-20 h-20 rounded-full border-4 border-white/30 shadow-md"
+                     className="mx-auto w-20 h-20 rounded-full border-4 border-white/30 shadow-md"
                    />
-                   <h2 className={`mt-4 text-2xl font-semibold ${isDark ? "text-white":"text-black"}`}>{singleGroupData.groupName}</h2>
-                   <p className={`text-sm  ${isDark ? "text-white/70" :"text-black"}`}>{singleGroupData.hobbyCategory}</p>
+                   <h2 className={`text-center mx-auto mt-4 text-2xl font-semibold ${isDark ? "text-white":"text-black"}`}>{singleGroupData.groupName}</h2>
+                   <p className={`text-center mx-auto text-sm  ${isDark ? "text-white/70" :"text-black"}`}>{singleGroupData.hobbyCategory}</p>
          
                    <div>
                       <p className={`mt-8 text-start text-sm ${isDark? "text-white":"text-black"}`}>
